67d04c66898ce64b210dfc6740a01fbc9620990e
Sara-Jo <sr930927@gmail.com>
2022-02-14 11:30:16 +0900

    first commit

---
+++ 7.문자열/2675문자열반복.py
@@ -0,0 +1,15 @@
+t = int(input())
+r = []
+s = []
+
+for i in range(t):
+    x, y = input().split()
+    r.append(x)
+    s.append(y)
+
+for i in range(t):
+    for c in s[i]:
+        for _ in range(int(r[i])):
+            print(c, end='')
+    print("")
+
