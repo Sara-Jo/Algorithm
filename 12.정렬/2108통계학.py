d402a9e6e92663218f66099b18bc338056e9c57f
Sara-Jo <sr930927@gmail.com>
2022-02-21 13:06:47 +0900

    2108통계학

--- 12.정렬/2108통계학.py
+++ 12.정렬/2108통계학.py
@@ -1,20 +1,19 @@
+import sys
+from collections import Counter
+
 n = int(input())
 arr = []
 
 for _ in range(n):
-    arr.append(int(input()))
-
-def most_often(arr):
-    max = 0
-    for i in arr:
-        cnt = arr.count(i)
-        if max < cnt:
-            max = cnt
-    return max
-
+    arr.append(int(sys.stdin.readline()))
 
 arr.sort()
 print(round(sum(arr) / n))  # 산술평균
 print(arr[n // 2])  # 중앙값
-print(most_often(arr))  # 최빈값
+# 최빈값
+cnt_arr = Counter(arr).most_common()
+if len(cnt_arr) > 1 and cnt_arr[0][1] == cnt_arr[1][1]:
+    print(cnt_arr[1][0])
+else:
+    print(cnt_arr[0][0])
 print(arr[n - 1] - arr[0])  # 범위
