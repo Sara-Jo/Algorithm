07685492c5cf2aa9d91c70851d0759f6b8ce57d2
Sara-Jo <sr930927@gmail.com>
2022-03-22 17:40:57 +0900

    11047

---
+++ 16.그리디알고리즘/11047.py
@@ -0,0 +1,12 @@
+n, k = map(int, input().split())
+coins = []
+for _ in range(n):
+    coins.append(int(input()))
+coins.sort(reverse=True)
+
+count = 0
+for i in coins:
+    count += k // i
+    k %= i   
+    
+print(count)
