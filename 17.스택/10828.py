12280ecca6959659055bdff918797177ca9a82b0
Sara-Jo <sr930927@gmail.com>
2022-03-28 19:20:01 +0900

    10828

---
+++ 17.스택/10828.py
@@ -0,0 +1,27 @@
+import sys
+input = sys.stdin.readline()
+
+n = int(input())
+arr = []
+
+for _ in range(n):
+    a = input().split()
+    if a[0] == 'push':
+        arr.append(a[1])
+    elif a[0] == 'pop':
+        if len(arr) == 0:
+            print(-1)
+        else:
+            print(arr.pop())
+    elif a[0] == 'size':
+        print(len(arr))
+    elif a[0] == 'empty':
+        if len(arr) == 0:
+            print(1)
+        else:
+            print(0)
+    elif a[0] == 'top':
+        if len(arr) == 0:
+            print(-1)
+        else:
+            print(arr[len(arr) - 1])
