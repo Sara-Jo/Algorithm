ca18d48ec60ac19a239a1b98dd2d4aa3ff804b05
Sara Jo <sjo@c4r6s5.42seoul.kr>
2022-03-30 11:51:30 +0900

    2164

---
+++ 18.큐,덱/2164.py
@@ -0,0 +1,11 @@
+from collections import deque
+
+n = int(input())
+cards = deque([i for i in range(1, n + 1)])
+
+while len(cards) != 1:
+    cards.popleft()
+    tmp = cards.popleft()
+    cards.append(tmp)
+
+print(cards[0])
