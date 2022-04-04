69a377d9b32c2d43a2991eb0b6489b49e3244ca7
Sara-Jo <sr930927@gmail.com>
2022-04-04 09:33:12 +0900

    2630

---
+++ 19.분할정복/2630.py
@@ -0,0 +1,26 @@
+n = int(input())
+paper = []
+for _ in range(n):
+    paper.append(list(map(int, input().split())))
+white = 0
+blue = 0
+
+def cut(x, y, n):
+    global white, blue
+    color = paper[x][y]
+    for i in range(x, x + n):
+        for j in range(y, y + n):
+            if color != paper[i][j]:
+                cut(x, y, n // 2)
+                cut(x, y + n // 2, n // 2)
+                cut(x + n // 2, y, n // 2)
+                cut(x + n // 2, y + n // 2, n // 2)
+                return
+    if color == 0:
+        white += 1
+    else:
+        blue += 1
+
+cut(0, 0, n)
+print(white)
+print(blue)
