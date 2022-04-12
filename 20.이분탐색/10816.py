b6e8bdbbc311baec58acdaa1c950836b5a645a93
Sara-Jo <sr930927@gmail.com>
2022-04-12 22:00:54 +0900

    modified 10816

--- 20.이분탐색/10816.py
+++ 20.이분탐색/10816.py
@@ -1,4 +1,7 @@
 # 1 ---------------------------------------------
+from selectors import EpollSelector
+
+
 n = int(input())
 a = list(map(int, input().split()))
 m = int(input())
@@ -19,6 +22,34 @@ for i in b:
 # m = int(input())
 # b = list(map(int, input().split()))
 
+# count = {}
+# for i in a:
+#     if i in count:
+#         count[i] += 1
+#     else:
+#         count[i] = 1
+
+# def binary_search(arr, target, start, end):
+#     if start > end:
+#         return 0
+#     mid = (start + end) // 2
+#     if arr[mid] == target:
+#         return count.get(target)
+#     elif arr[mid] < target:
+#         return binary_search(arr, target, mid + 1, end)
+#     else:
+#         return binary_search(arr, target, start, mid - 1)
+
+# for i in b:
+#     print(binary_search(a, i, 0, n - 1), end=' ')
+
+# 3 ---------------------------------------------
+# n = int(input())
+# a = list(map(int, input().split()))
+# a.sort()
+# m = int(input())
+# b = list(map(int, input().split()))
+
 # dic = dict()
 
 # for i in a:
@@ -33,7 +64,7 @@ for i in b:
 #     else:
 #         print(0, end=' ')
 
-# 3 ---------------------------------------------
+# 4 ---------------------------------------------
 # from collections import Counter
 
 # n = int(input())
