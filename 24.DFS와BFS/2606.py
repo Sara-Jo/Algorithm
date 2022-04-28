e7d22aece2734ef9fd0c657dfca55415f8ffafaf
Sara-Jo <sr930927@gmail.com>
2022-04-29 01:00:00 +0900

    2606 bfs 추가

--- 24.DFS와BFS/2606.py
+++ 24.DFS와BFS/2606.py
@@ -19,3 +19,32 @@ def dfs(v):
 
 dfs(1)
 print(cnt)
+
+
+# from collections import deque
+
+# n = int(input())
+# m = int(input())
+# graph = [[] for _ in range(n + 1)]
+# visited = [False] * (n + 1)
+# cnt = 0
+
+# for _ in range(m):
+#     a, b = map(int, input().split())
+#     graph[a].append(b)
+#     graph[b].append(a)
+
+# def bfs(v):
+#     global cnt
+#     queue = deque([v])
+#     visited[v] = True
+#     while queue:
+#         x = queue.popleft()
+#         for i in graph[x]:
+#             if not visited[i]:
+#                 queue.append(i)
+#                 visited[i] = True
+#                 cnt += 1
+
+# bfs(1)
+# print(cnt)
