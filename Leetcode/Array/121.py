7002251ee4013bd442b332d65595d7b91b7da95b
Sara-Jo <sr930927@gmail.com>
2022-12-22 11:16:36 +0900

    leetcode 121

---
+++ Leetcode/Array/121.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+
+        min_price = 1e9
+        max_profit = 0
+        for i in range(len(prices)):
+            if prices[i] < min_price:
+                min_price = prices[i]
+            else:
+                max_profit = max(max_profit, prices[i] - min_price)
+        return max_profit
