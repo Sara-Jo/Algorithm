d90763930144cf0269ddcb0c9e68086140cdbda5
Sara-Jo <sr930927@gmail.com>
2023-01-11 21:16:00 +0900

    leetcode 79

--- Leetcode/Matrix/79.py
+++ Leetcode/Matrix/79.py
@@ -1,31 +1,26 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         for i in range(len(board)):
-            for j, c in enumerate(board[i]):
-                if c == word[0]:
-                    if self.bfs(i, j, board, word) == True:
+            for j in range(len(board[0])):
+                if board[i][j] == word[0]:
+                    board[i][j] = '#'
+                    if self.dfs(i, j, board, word[1:]):
                         return True
+                    board[i][j] = word[0]
         return False
 
-    def bfs(self, x, y, board, word):
-
+    def dfs(self, x, y, board, word):
         dx = [0, 0, 1, -1]
         dy = [1, -1, 0, 0]
-        n = len(board)
-        m = len(board[0])
 
-        q = deque([(x, y, 0, [(x, y)])])
-
-        while q:
-            x, y, count, visited = q.popleft()
-            if count == len(word) - 1:
-                return True
-            for i in range(4):
-                nx = x + dx[i]
-                ny = y + dy[i]
-                if 0 <= nx < n and 0 <= ny < m and board[nx][ny] == word[count + 1] and (nx, ny) not in visited:
-                    tmp = visited.copy()
-                    tmp.append((nx, ny))
-                    q.append((nx, ny, count + 1, tmp))
-            
+        if len(word) == 0:
+            return True
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < len(board) and 0 <= ny < len(board[0]) and board[nx][ny] == word[0]:
+                board[nx][ny] = '#'
+                if self.dfs(nx, ny, board, word[1:]):
+                    return True
+                board[nx][ny] = word[0]
         return False
