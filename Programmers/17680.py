9fe39c8d77487ffcbc5afb89876fdf2d8deefa3f
Sara-Jo <sr930927@gmail.com>
2023-02-12 14:13:15 +0900

    programmers 17680

---
+++ Programmers/17680.py
@@ -0,0 +1,18 @@
+def solution(cacheSize, cities):
+    if cacheSize == 0:
+        return len(cities) * 5
+    
+    answer = 0 
+    cache = []
+    for city in cities:
+        city = city.lower()
+        if city in cache:
+            cache.remove(city)
+            answer += 1
+        else:
+            if len(cache) >= cacheSize:
+                cache.pop(0)
+            answer += 5
+        cache.append(city)
+               
+    return answer
