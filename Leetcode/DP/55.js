5721c4a57519f3bae0c5b4e84caf052b1b9dacba
Sara-Jo <sarajo.dev@gmail.com>
2024-10-21 19:07:24 +0900

    leetcode 55

---
+++ Leetcode/DP/55.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canJump = function (nums) {
+  const n = nums.length;
+  const dp = Array(n).fill(false);
+  dp[0] = true;
+
+  for (let i = 0; i < n; i++) {
+    const maxJump = nums[i];
+    if (!dp[i]) continue;
+
+    for (let j = 1; j <= maxJump; j++) {
+      if (i + j < n) {
+        dp[i + j] = true;
+        if (i + j === n - 1) return true;
+      }
+    }
+  }
+
+  return dp[n - 1];
+};
