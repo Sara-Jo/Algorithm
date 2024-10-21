9d6c3a5d28f0f20d9e74f1ea5c2d177e9633685f
Sara-Jo <sarajo.dev@gmail.com>
2024-10-21 19:13:19 +0900

    leetcode 55 better approach

--- Leetcode/DP/55.js
+++ Leetcode/DP/55.js
@@ -3,21 +3,30 @@
  * @return {boolean}
  */
 var canJump = function (nums) {
-  const n = nums.length;
-  const dp = Array(n).fill(false);
-  dp[0] = true;
+  //   const n = nums.length;
+  //   const dp = Array(n).fill(false);
+  //   dp[0] = true;
 
-  for (let i = 0; i < n; i++) {
-    const maxJump = nums[i];
-    if (!dp[i]) continue;
+  //   for (let i = 0; i < n; i++) {
+  //     const maxJump = nums[i];
+  //     if (!dp[i]) continue;
 
-    for (let j = 1; j <= maxJump; j++) {
-      if (i + j < n) {
-        dp[i + j] = true;
-        if (i + j === n - 1) return true;
-      }
-    }
+  //     for (let j = 1; j <= maxJump; j++) {
+  //       if (i + j < n) {
+  //         dp[i + j] = true;
+  //         if (i + j === n - 1) return true;
+  //       }
+  //     }
+  //   }
+
+  //   return dp[n - 1];
+
+  let m = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (i > m) return false;
+
+    m = Math.max(m, i + nums[i]);
   }
 
-  return dp[n - 1];
+  return true;
 };
